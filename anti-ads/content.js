e6c416f18725ad5d3a743f461128a2abb3c570dc
3axap4eHko <3axap4eHko@gmail.com>
2014-08-21 03:01:23 +0300

    fix

--- anti-ads/content.js
+++ anti-ads/content.js
@@ -1,4 +1,4 @@
-    chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    chrome && chrome.runtime && chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         if (msg && msg.action=='remove') {
             msg.selector.forEach(function(selector){
                 var matches = toArray(document.querySelectorAll(selector));
